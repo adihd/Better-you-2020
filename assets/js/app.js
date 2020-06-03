fc0ba551c9115c54ede49119a134ae092a3bfb64
adihd <adi.straussa@gmail.com>
2020-06-03 18:40:06 +0000

    גרסאת מוביל

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,6 +5,35 @@
  Module/App: App Js
  */
 
+
+// Set the date we're counting down to
+var countDownDate = new Date("June 30, 2020 15:37:25").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="demo"
+  document.getElementById("countdown").innerHTML = days + " Days";
+
+  // If the count down is finished, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("countdown").innerHTML = "EXPIRED";
+  }
+}, 1000);
+
 window.onload = function () {
 
 var chart = new CanvasJS.Chart("chartContainer", {
@@ -146,3 +175,5 @@ chart.render();
     init();
 
 })(jQuery)
+
+
